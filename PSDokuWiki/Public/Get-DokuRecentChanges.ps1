d7218592544d67779f0d50b2652ebb68e5f9119c
Andy de la Pole <andrew.delapole@bcpartners.com>
2018-07-17 16:11:41 +0100

    .

--- PSDokuWiki/Public/Get-DokuRecentChanges.ps1
+++ PSDokuWiki/Public/Get-DokuRecentChanges.ps1
@@ -2,27 +2,27 @@
 <#
 	.SYNOPSIS
 		Returns a list of recent changes since given timestamp
-	
+
 	.DESCRIPTION
 		Returns a list of recent changes since given timestamp.
 		As stated in recent_changes: Only the most recent change for each page is listed, regardless of how many times that page was changed
-	
+
 	.PARAMETER DokuSession
 		The DokuSession from which to get the changes
-	
+
 	.PARAMETER VersionTimestamp
 		Get all pages since this timestamp
-	
+
 	.EXAMPLE
 		PS C:\> Get-DokuRecentChanges -DokuSession $DokuSession -VersionTimestamp $VersionTimestamp
-	
+
 	.OUTPUTS
 		System.Management.Automation.PSObject[]
-	
+
 	.NOTES
 		AndyDLP - 2018-05-26
 #>
-	
+
 	[CmdletBinding(PositionalBinding = $true)]
 	[OutputType([psobject[]])]
 	param
@@ -38,14 +38,14 @@
 		[ValidateNotNullOrEmpty()]
 		[int]$VersionTimestamp
 	)
-	
+
 	$payload = (ConvertTo-XmlRpcMethodCall -Name "wiki.getRecentChanges" -Params $VersionTimestamp) -replace "Int32", "i4"
 	if ($DokuSession.SessionMethod -eq "HttpBasic") {
 		$httpResponse = Invoke-WebRequest -Uri $DokuSession.TargetUri -Method Post -Headers $DokuSession.Headers -Body $payload -ErrorAction Stop
 	} else {
 		$httpResponse = Invoke-WebRequest -Uri $DokuSession.TargetUri -Method Post -Headers $DokuSession.Headers -Body $payload -ErrorAction Stop -WebSession $DokuSession.WebSession
 	}
-	
+
 	$MemberNodes = ([xml]$httpResponse.Content | Select-Xml -XPath "//struct").Node
 	foreach ($node in $MemberNodes) {
 		$ChangeObject = New-Object PSObject -Property @{
@@ -56,5 +56,5 @@
 		}
 		[array]$PageChanges = $PageChanges + $ChangeObject
 	}
-	return $PageChanges
+	$PageChanges
 }
