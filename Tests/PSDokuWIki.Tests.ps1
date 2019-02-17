06121b3aa6c472dde57e337f0d5146ce6bf212c4
Andy de la Pole <adlp@homenet.adlp.tech>
2019-02-17 11:05:52 +0000

    .

--- Tests/PSDokuWIki.Tests.ps1
+++ Tests/PSDokuWIki.Tests.ps1
@@ -86,13 +86,15 @@ Describe 'New-DokuSession' {
             {New-DokuSession -Server $Server -Unencrypted -SessionMethod 'Hello World' -Credential $credential} | Should -Throw
         }
         It 'Successfully returns an object with the correct primary type name' {
-            Mock Invoke-WebRequest { return "" }
+            function Invoke-WebRequest {  }
+            Mock Invoke-WebRequest { return "nothing" }
             # TODO: 
             #  Do I need a class to do -BeOfType [DokuWiki.Session.Detail]
             (New-DokuSession -Server $Server -Credential $credential).PSTypeNames[0] | Should -Be 'DokuWiki.Session.Detail'
         }
         It 'Successfully returns an object with all the correct properties' {
-            Mock Invoke-WebRequest { return "" }
+            function Invoke-WebRequest {  }
+            Mock Invoke-WebRequest { return "nothing" }
             $SessionObjectProperties = (New-DokuSession -Server $Server -Credential $credential).PSObject.Properties.Name 
             @('Server','TargetUri','SessionMethod','Headers','WebSession','TimeStamp','UnencryptedEndpoint') | Where-Object -FilterScript { $SessionObjectProperties -notcontains $_ } | Should -BeNullOrEmpty
         }
