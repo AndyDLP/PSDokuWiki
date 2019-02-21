aa92a85b50b7e5f6b5cb908dc87fd23e4f3c9be9
Andy de la Pole <andrew.delapole@bcpartners.com>
2019-02-21 10:54:26 +0000

    Added IsConnected parameter

--- PSDokuWiki/Public/Get-DokuServer.ps1
+++ PSDokuWiki/Public/Get-DokuServer.ps1
@@ -5,12 +5,15 @@ function Get-DokuServer {
 
 	.DESCRIPTION
         Gets any current connection to a DokuWiki API
+
+	.PARAMETER IsConnected
+		Only return TRUE if currently connected and FALSE if not
         
 	.EXAMPLE
 		PS C:\> Get-DokuServer | Format-List -Property *
 
 	.OUTPUTS
-		DokuWiki.Session.Detail,PSCustomObject
+		DokuWiki.Session.Detail,Boolean
 
 	.NOTES
 		AndyDLP - 2019
@@ -19,13 +22,26 @@ function Get-DokuServer {
 		https://github.com/AndyDLP/PSDokuWiki
 #>
     [CmdletBinding()]
-    param ()
+    param
+    (
+		[Parameter(Mandatory = $false,
+                   Position = 1,
+                   HelpMessage = 'Only return TRUE if currently connected')]
+        [switch]$IsConnected
+    )
 
     if ($null -ne $Script:DokuServer) {
         Write-Verbose "Currently connected to DokuWiki server: $($Script:DokuServer.TargetUri)"
-        $Output += $Script:DokuServer
-        return $Output
+        if ($IsConnected) {
+            $true
+        } else {
+            $Output += $Script:DokuServer
+            $Output
+        }
     } else {
         Write-Verbose "Not currently connected to any DokuWiki servers"
+        if ($IsConnected) {
+            $false
+        }
     }
 }
