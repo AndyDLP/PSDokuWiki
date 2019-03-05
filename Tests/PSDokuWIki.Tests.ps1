416a4eb3bb75eca91911c27e0bddd4ca17a51cb0
Andy de la Pole <andydlp93@gmail.com>
2019-03-05 12:14:08 +0000

    Added more tests to Add-DokuAclRule

--- Tests/PSDokuWIki.Tests.ps1
+++ Tests/PSDokuWIki.Tests.ps1
@@ -247,7 +247,6 @@ Describe 'Add-DokuAclRule' {
         Set-StrictMode -Version latest
         InModuleScope PSDokuWiki {
             It 'Should display the exception message' {
-                
                 Mock Invoke-DokuApiCall {
                     return (
                         [PSCustomObject]@{
@@ -281,8 +280,59 @@ Describe 'Add-DokuAclRule' {
                         }
                     )
                 }
-                Add-DokuAclRule -FullName "namespace:pagename" -Principal "username" -Acl 2 -ErrorVariable DokuErrorVariable
+                Add-DokuAclRule -FullName "namespace:pagename" -Principal "username" -Acl 2 -ErrorAction SilentlyContinue -ErrorVariable DokuErrorVariable
                 $DokuErrorVariable.exception.message | Should -Be 'Failed to apply Acl: 2 for user: username to entity: namespace:pagename'
+            }
+        }
+    }
+    Context 'When one user is passed to one page' {
+        Set-StrictMode -Version latest
+        InModuleScope PSDokuWiki {
+            It 'Should make one call to Invoke-DokuApiCall' {
+                Mock Invoke-DokuApiCall {
+                    return (
+                        [PSCustomObject]@{
+                            CompletedSuccessfully = $true
+                            XMLPayloadResponse = [xml]'<?xml version="1.0"?><methodResponse><value><boolean>1</boolean></value></methodResponse>'
+                        }
+                    )
+                }
+                Add-DokuAclRule -FullName "namespace:pagename" -Principal "username" -Acl 2
+                Assert-MockCalled -CommandName Invoke-DokuApiCall -Exactly -Times 1
+            }
+        }
+    }
+    Context 'When two users are passed to one page' {
+        Set-StrictMode -Version latest
+        InModuleScope PSDokuWiki {
+            It 'Should make two calls to Invoke-DokuApiCall' {
+                Mock Invoke-DokuApiCall {
+                    return (
+                        [PSCustomObject]@{
+                            CompletedSuccessfully = $true
+                            XMLPayloadResponse = [xml]'<?xml version="1.0"?><methodResponse><value><boolean>1</boolean></value></methodResponse>'
+                        }
+                    )
+                }
+                Add-DokuAclRule -FullName 'namespace:pagename' -Principal 'username','username2' -Acl 2
+                Assert-MockCalled -CommandName Invoke-DokuApiCall -Exactly -Times 2
+            }
+        }
+    }
+    Context 'When two users are passed to two pages' {
+        Set-StrictMode -Version latest
+        InModuleScope PSDokuWiki {
+            It 'Should make four calls to Invoke-DokuApiCall' {
+                Mock Invoke-DokuApiCall {
+                    return (
+                        [PSCustomObject]@{
+                            CompletedSuccessfully = $true
+                            XMLPayloadResponse = [xml]'<?xml version="1.0"?><methodResponse><value><boolean>1</boolean></value></methodResponse>'
+                        }
+                    )
+                }
+                Add-DokuAclRule -FullName 'namespace:pagename','pagename' -Principal 'username','username2' -Acl 2
+                Assert-MockCalled -CommandName Invoke-DokuApiCall -Exactly -Times 4
             }
         }
     }
