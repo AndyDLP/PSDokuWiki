6c21e8ecfc4c187c6be482b83b25dff9513b1492
Andy de la Pole <andydlp93@gmail.com>
2019-03-07 10:35:06 +0000

    Added code coverage info

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -3,10 +3,10 @@
 # It should invoke on PowerShell v2 and later
 # We serialize XML results and pull them in appveyor.yml
 
-#If Finalize is specified, we collect XML output, upload tests, and indicate build errors
+# If Finalize is specified, we collect XML output, upload tests, and indicate build errors
 param([switch]$Finalize)
 
-#Initialize some variables, move to the project root
+# Initialize some variables, move to the project root
     $PSVersion = $PSVersionTable.PSVersion.Major
     $TestFile = "TestResultsPS$PSVersion.xml"
     $ProjectRoot = $ENV:APPVEYOR_BUILD_FOLDER
@@ -15,78 +15,131 @@ param([switch]$Finalize)
     Write-Host "Build version :`  $env:APPVEYOR_BUILD_VERSION"
     Write-Host "Branch        :`  $env:APPVEYOR_REPO_BRANCH"
 
-#Run a test with the current version of PowerShell
-    if(-not $Finalize)
-    {
-        "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
+# Run a test with the current version of PowerShell
+if(-not $Finalize) {
+    "`n`tSTATUS: Testing with PowerShell $PSVersion`n"
+
+    Import-Module (Resolve-Path 'C:\Program Files\WindowsPowerShell\Modules\Pester\*\Pester.psd1')
+    Import-Module (Join-Path -Path $ProjectRoot -ChildPath 'PSDokuWiki') -Force
+
+    Invoke-Pester -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru | Export-Clixml -Path "$ProjectRoot\PesterResults$PSVersion.xml"
+} else {
+    # If finalize is specified, check for failures and 
+    # Show status...
+        $AllFiles = Get-ChildItem -Path $ProjectRoot\*Results*.xml | Select-Object -ExpandProperty FullName
+        "`n`tSTATUS: Finalizing results`n"
+        "COLLATING FILES:`n$($AllFiles | Out-String)"
+
+    # Upload results for test page
+        Get-ChildItem -Path "$ProjectRoot\TestResultsPS*.xml" | Foreach-Object -Process {
     
-        Import-Module (Resolve-Path 'C:\Program Files\WindowsPowerShell\Modules\Pester\*\Pester.psd1')
+            $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+            $Source = $_.FullName
 
-        $TopLevelFolder = Split-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path -Parent) -Parent
-        Import-Module (Join-Path -Path $TopLevelFolder -ChildPath 'PSDokuWiki\PSDokuWiki.psm1') -Force
+            "UPLOADING FILES: $Address $Source"
 
-        Invoke-Pester -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -PassThru | Export-Clixml -Path "$ProjectRoot\PesterResults$PSVersion.xml"
-    }
+            (New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
+        }
 
-#If finalize is specified, check for failures and 
-    else
-    {
-        #Show status...
-            $AllFiles = Get-ChildItem -Path $ProjectRoot\*Results*.xml | Select-Object -ExpandProperty FullName
-            "`n`tSTATUS: Finalizing results`n"
-            "COLLATING FILES:`n$($AllFiles | Out-String)"
-
-        #Upload results for test page
-            Get-ChildItem -Path "$ProjectRoot\TestResultsPS*.xml" | Foreach-Object -Process {
+    # What failed?
+        $Results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults*.xml" | Import-Clixml )
         
-                $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
-                $Source = $_.FullName
+        $FailedCount = $Results | Select-Object -ExpandProperty FailedCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum
 
-                "UPLOADING FILES: $Address $Source"
+        if ($FailedCount -gt 0) {
 
-                (New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
-            }
+            $FailedItems = $Results | Select-Object -ExpandProperty TestResult | Where-Object -FilterScript {$_.Passed -notlike $True}
 
-        #What failed?
-            $Results = @( Get-ChildItem -Path "$ProjectRoot\PesterResults*.xml" | Import-Clixml )
-            
-            $FailedCount = $Results | Select-Object -ExpandProperty FailedCount | Measure-Object -Sum | Select-Object -ExpandProperty Sum
-    
-            if ($FailedCount -gt 0) {
+            "FAILED TESTS SUMMARY:`n"
+            $FailedItems | ForEach-Object -Process {
+                $Test = $_
+                [pscustomobject]@{
+                    Describe = $Test.Describe
+                    Context = $Test.Context
+                    Name = "It $($Test.Name)"
+                    Result = $Test.Result
+                }
+            } |
+                Sort-Object -Property Describe, Context, Name, Result | Format-List
 
-                $FailedItems = $Results | Select-Object -ExpandProperty TestResult | Where-Object -FilterScript {$_.Passed -notlike $True}
-
-                "FAILED TESTS SUMMARY:`n"
-                $FailedItems | ForEach-Object -Process {
-                    $Test = $_
-                    [pscustomobject]@{
-                        Describe = $Test.Describe
-                        Context = $Test.Context
-                        Name = "It $($Test.Name)"
-                        Result = $Test.Result
-                    }
-                } |
-                    Sort-Object -Property Describe, Context, Name, Result | Format-List
-
-                throw "$FailedCount tests failed."
-            }
-
-        #ScriptAnalyzer
-        "`n`tScriptAnalyzer: checking code...`n"
-        Add-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Running
-        $CodeResults = Invoke-ScriptAnalyzer -Path $ProjectRoot\PSDokuWiki -Recurse -Severity Error -ErrorAction SilentlyContinue
-        If ($CodeResults) {
-            $ResultString = $CodeResults | Out-String
-            Write-Warning $ResultString
-            Add-AppveyorMessage -Message "PSScriptAnalyzer output contained one or more result(s) with 'Error' severity.`
-            Check the 'Tests' tab of this build for more details." -Category Error
-            Update-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Failed -ErrorMessage $ResultString
-            
-            # Failing the build
-            Throw "Build failed"
+            throw "$FailedCount tests failed."
         }
-        Else {
-            "CODE ANALYSIS COMPLETE:`n`nNO ERRORS"
-            Update-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Passed
-        }
+
+    # ScriptAnalyzer
+    "`n`tScriptAnalyzer: checking code...`n"
+    Add-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Running
+    $CodeResults = Invoke-ScriptAnalyzer -Path $ProjectRoot\PSDokuWiki -Recurse -Severity Error -ErrorAction SilentlyContinue
+    If ($CodeResults) {
+        $ResultString = $CodeResults | Out-String
+        Write-Warning $ResultString
+        Add-AppveyorMessage -Message "PSScriptAnalyzer output contained one or more result(s) with 'Error' severity.`
+        Check the 'Tests' tab of this build for more details." -Category Error
+        Update-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Failed -ErrorMessage $ResultString
+        
+        # Failing the build
+        Throw "Build failed"
     }
+    Else {
+        "`nCODE ANALYSIS COMPLETE:`n`tNO ERRORS`n"
+        Update-AppveyorTest -Name "PsScriptAnalyzer" -Outcome Passed
+    }
+
+    #region Show coverage
+    # https://dille.name/blog/2017/06/29/code-coverage-metrics-using-pester-for-powershell-modules/
+    $TestResults = Invoke-Pester -Path ".\Tests" -CodeCoverage ".\PSDokuWiki\*\*.ps1" -PassThru -Show None
+
+    $CodeCoverage = @{
+        Functions = @{}
+        Statement = @{
+            Analyzed = $TestResults.CodeCoverage.NumberOfCommandsAnalyzed
+            Executed = $TestResults.CodeCoverage.NumberOfCommandsExecuted
+            Missed   = $TestResults.CodeCoverage.NumberOfCommandsMissed
+            Coverage = 0
+        }
+        Function = @{}
+    }
+    $CodeCoverage.Statement.Coverage = [math]::Round($CodeCoverage.Statement.Executed / $CodeCoverage.Statement.Analyzed * 100, 2)
+
+    $TestResults.CodeCoverage.HitCommands | Group-Object -Property Function | ForEach-Object {
+        if (-Not $CodeCoverage.Functions.ContainsKey($_.Name)) {
+            $CodeCoverage.Functions.Add($_.Name, @{
+                Name     = $_.Name
+                Analyzed = 0
+                Executed = 0
+                Missed   = 0
+                Coverage = 0
+            })
+        }
+
+        $CodeCoverage.Functions[$_.Name].Analyzed += $_.Count
+        $CodeCoverage.Functions[$_.Name].Executed += $_.Count
+    }
+    $TestResults.CodeCoverage.MissedCommands | Group-Object -Property Function | ForEach-Object {
+        if (-Not $CodeCoverage.Functions.ContainsKey($_.Name)) {
+            $CodeCoverage.Functions.Add($_.Name, @{
+                Name     = $_.Name
+                Analyzed = 0
+                Executed = 0
+                Missed   = 0
+                Coverage = 0
+            })
+        }
+
+        $CodeCoverage.Functions[$_.Name].Analyzed += $_.Count
+        $CodeCoverage.Functions[$_.Name].Missed   += $_.Count
+    }
+    foreach ($function in $CodeCoverage.Functions.Values) {
+        $function.Coverage = [math]::Round($function.Executed / $function.Analyzed * 100)
+    }
+    $CodeCoverage.Function = @{
+        Analyzed = $CodeCoverage.Functions.Count
+        Executed = ($CodeCoverage.Functions.Values | Where-Object { $_.Executed -gt 0 }).Length
+        Missed   = ($CodeCoverage.Functions.Values | Where-Object { $_.Executed -eq 0 }).Length
+    }
+    $CodeCoverage.Function.Coverage = [math]::Round($CodeCoverage.Function.Executed / $CodeCoverage.Function.Analyzed * 100, 2)
+    "Statement coverage: $($CodeCoverage.Statement.Analyzed) analyzed, $($CodeCoverage.Statement.Executed) executed, $($CodeCoverage.Statement.Missed) missed, $($CodeCoverage.Statement.Coverage)%."
+    "Function coverage: $($CodeCoverage.Function.Analyzed) analyzed, $($CodeCoverage.Function.Executed) executed, $($CodeCoverage.Function.Missed) missed, $($CodeCoverage.Function.Coverage)%."
+    #endregion
+
+    # Create Appveyor test and mark as done?
+}
