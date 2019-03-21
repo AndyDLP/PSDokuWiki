160f52900cb4b108894355173280eceac89cda84
AndyDLP <andydlp93@gmail.com>
2019-03-21 21:21:27 +0000

    updated NuGet API key

--- PSDokuWiki.PSDeploy.ps1
+++ PSDokuWiki.PSDeploy.ps1
@@ -21,8 +21,6 @@
 
 # Publish to gallery with a few restrictions
 if($env:BHProjectName -and $env:BHProjectName.Count -eq 1 -and $env:BHBuildSystem -ne 'Unknown' -and $env:BHBranchName -eq "master" -and $env:BHCommitMessage -match '!deploy') {
-    Write-Host "Would deploy to PSGallery"
-    <#
     Deploy Module {
         By PSGalleryModule {
             FromSource $ENV:BHProjectName
@@ -32,7 +30,6 @@ if($env:BHProjectName -and $env:BHProjectName.Count -eq 1 -and $env:BHBuildSyste
             }
         }
     } @Verbose
-    #>
 } else {
     "Skipping deployment: To deploy, ensure that...`n" + "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" + "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" + "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" | Write-Host
 }
