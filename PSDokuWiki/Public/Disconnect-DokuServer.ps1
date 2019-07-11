baff503758e392c706980406a26cff3c04da4842
David Darus <darusdp@gmail.com>
2019-07-11 09:47:03 -0400

    Update Disconnect-DokuServer.ps1

--- PSDokuWiki/Public/Disconnect-DokuServer.ps1
+++ PSDokuWiki/Public/Disconnect-DokuServer.ps1
@@ -1,11 +1,17 @@
 function Disconnect-DokuServer {
     [CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='Low')]
-    param()
+    param(
+        [Parameter(Mandatory = $false,
+            Position = 1,
+            HelpMessage = 'Bypass confirmations of calls during this connect/disconnect session')]
+        [ValidateNotNullOrEmpty()]
+        [switch]$BypassConfirm
+    )
 
     begin {}
 
     process {
-        if ($PSCmdlet.ShouldProcess("Disconnect from current server: $($Script:DokuServer.TargetUri)")) {
+        if ($BypassConfirm -or $PSCmdlet.ShouldProcess("Disconnect from current server: $($Script:DokuServer.TargetUri)")) {
             if ($null -ne $Script:DokuServer) {
                 Write-Verbose "Disconnecting DokuWiki instance: $($Script:DokuServer.TargetUri)"
                 $Script:DokuServer = $null
