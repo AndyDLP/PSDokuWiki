ba4ad241ec74ed4b3e7e19971d61a63d7d043876
AndyDLP <andydlp93@gmail.com>
2021-09-12 18:03:30 +0100

    Remove unnecessary overwrite of auto var

--- psake.ps1
+++ psake.ps1
@@ -284,8 +284,6 @@ Task Deploy -depends PostBuildTest {
     $lines
 
     # Publish to gallery with a few restrictions
-    $isWindows = (Get-CimInstance -ClassName 'CIM_OperatingSystem').Caption -match 'Windows'
-    Write-Verbose "isWindows: $isWindows"
     if (($env:BHProjectName) -and ($env:BHProjectName.Count -eq 1) -and ($env:BHBuildSystem -ne 'Unknown') -and ($env:BHBranchName -eq 'master') -and ($env:BHCommitMessage -match '!deploy') -and ($isWindows -eq $true)) {
         Write-Host 'Deploying to PSGallery...'
         Publish-Module -Path $ENV:BHPSModulePath -Repository 'PSGallery' -NuGetApiKey $ENV:NugetApiKey -ErrorAction Stop @Verbose
